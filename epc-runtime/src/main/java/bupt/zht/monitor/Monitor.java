ab371db90626b9c0aea3ed5da28ffa1c7142cdfe
zhanghangting <1017452493qq.com>
2018-11-19 17:10:22 +0800

    完成准备工作，可以开始更新逻辑树模块的开发

--- epc-runtime/src/main/java/bupt/zht/monitor/Monitor.java
+++ epc-runtime/src/main/java/bupt/zht/monitor/Monitor.java
@@ -3,5 +3,7 @@ package bupt.zht.monitor;
 public class Monitor {
     // 用于监听发布订阅系统事件
     public static void main(String[] args){
+        // 在update-logictree分支上进行开发，以后不管在mac还是在windows下
+        // 只需要先fetch一下 merge本地的代码开发即可
     }
 }
