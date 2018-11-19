4377fe4fe6c35d9ce7245adbca28baa8f43b7f62
zhanghangting <1017452493qq.com>
2018-11-19 17:28:05 +0800

    hello mac

--- epc-runtime/src/main/java/bupt/zht/monitor/Monitor.java
+++ epc-runtime/src/main/java/bupt/zht/monitor/Monitor.java
@@ -6,5 +6,6 @@ public class Monitor {
         // 在update-logictree分支上进行开发，以后不管在mac还是在windows下
         // 只需要先fetch一下 merge本地的代码开发即可
         System.out.print("hello windows");
+        System.out.println("hello mac");
     }
 }
