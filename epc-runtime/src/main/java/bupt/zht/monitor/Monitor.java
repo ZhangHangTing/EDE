f9cc4f0cb029dd9c2e9fc5e076e2f335f2854705
kobe <1017452493@qq.com>
2018-11-19 17:16:51 +0800

    mac开发更新逻辑树模块的测试

--- epc-runtime/src/main/java/bupt/zht/monitor/Monitor.java
+++ epc-runtime/src/main/java/bupt/zht/monitor/Monitor.java
@@ -5,5 +5,6 @@ public class Monitor {
     public static void main(String[] args){
         // 在update-logictree分支上进行开发，以后不管在mac还是在windows下
         // 只需要先fetch一下 merge本地的代码开发即可
+        System.out.print("hello windows");
     }
 }
