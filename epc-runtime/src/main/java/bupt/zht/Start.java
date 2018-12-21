007aacc9b74a5b4fb773a9d31aacd0281c536234
kobe <1017452493@qq.com>
2018-12-21 16:00:29 +0800

    测试创建多流程实例

--- epc-runtime/src/main/java/bupt/zht/Start.java
+++ epc-runtime/src/main/java/bupt/zht/Start.java
@@ -1,6 +1,7 @@
 package bupt.zht;
 
 import bupt.zht.monitor.Monitor;
+import bupt.zht.process.ProcessInstance;
 import bupt.zht.process.ProcessModel;
 import org.dom4j.DocumentException;
 
@@ -13,14 +14,14 @@ public class Start {
         EpcCompiler epcCompiler = new EpcCompiler();
         String path = System.getProperty("user.dir") + "/epc-compile/src/main/resources/";
         epcCompiler.compile(epcCompiler,path + "ProductAndAssemble.xml");
-//        System.out.print("EPML流程模型已经解析完毕，输出相关信息：");
-//        int i = 1;
-//        for(ProcessModel processModel : ProcessInfo.processModelList){
-//            System.out.println("第 " + i + "个流程模型：" + processModel.getProcessModelId());
-//            processModel.getEpcCompiler().showEventFunctionMap();
-//            processModel.getEpcCompiler().showFunctionLogicTree();
-//            processModel.getEpcCompiler().showLogicUnitEventsMap();
-//        }
+        System.out.print("EPML流程模型已经解析完毕，输出相关信息：");
+        int i = 1;
+        for(ProcessModel processModel : ProcessInfo.processModelList){
+            System.out.println("第 " + i + "个流程模型：" + processModel.getProcessModelId());
+            processModel.getEpcCompiler().showEventFunctionMap();
+            processModel.getEpcCompiler().showFunctionLogicTree();
+            processModel.getEpcCompiler().showLogicUnitEventsMap();
+        }
 
 //        epcCompiler.mappingLogicEvent();
 //        epcCompiler.showLogicUnitEventsMap();
@@ -28,14 +29,20 @@ public class Start {
 //        epcCompiler.showFunctionLogicTree();
 //        epcCompiler.showEventFunctionMap();
 //        ProcessInfo.epcCompiler = epcCompiler;
-//        Monitor.start();
-//        while (true) {
-//            System.out.println("running");
-//            try {
-//                Thread.sleep(3000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        System.out.println("接下来监听流程实例事件信息..................");
+        Monitor.start();
+        while (true) {
+            for(ProcessModel processModel : ProcessInfo.processModelList){
+                System.out.println("打印当前" + processModel.getProcessModelId() + "流程模型中的流程实例：");
+                for(ProcessInstance processInstance : processModel.getProcessInstancesList()){
+                    System.out.println(processInstance.getProcessInstanceId());
+                }
+            }
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
